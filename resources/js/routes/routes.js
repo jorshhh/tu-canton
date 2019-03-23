2220c7bedfb9667646881b0ee7bee1c24673e871
Jorsh <jrangel@aidc.mx>
2019-03-22 18:04:18 -0600

    Vue and DB initialized@

---
+++ resources/js/routes/routes.js
@@ -0,0 +1,9 @@
+import HomeComponent from '../components/HomeComponent.vue';
+
+export const routes = [
+    {
+        path: '/',
+        component: HomeComponent,
+        name: 'Home'
+    },
+];
